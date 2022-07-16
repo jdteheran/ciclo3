986251bb2486b15d7f0561a3e64f9b6897ca8ac2
Juan Teheran <jdteheran@Juans-MacBook-Pro.local>
2022-07-16 08:30:37 -0500

    js

--- assets/js/script.js
+++ assets/js/script.js
@@ -11,3 +11,25 @@ btn_agregar.addEventListener('click', () => {
 
 })
 
+let btn_formulario = document.querySelector('.btn-formulario')
+
+let titulo_formulario = document.querySelector('.titulo-formulario')
+titulo_formulario.style.display = 'none'
+
+btn_formulario.addEventListener('click', () => {
+    let inputs_formulario = document.querySelector('.inputs-formulario')
+
+    if (titulo_formulario.style.display == 'none') {
+        titulo_formulario.style.display = 'block'
+        inputs_formulario.style.display = 'block'
+
+        btn_formulario.innerHTML = 'Cancelar'
+    } else {
+        titulo_formulario.style.display = 'none'
+        inputs_formulario.style.display = 'none'
+
+        btn_formulario.innerHTML = 'Agregar manga'
+    }
+
+    
+})
